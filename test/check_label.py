2b9fca7a263780b5e3bafe60537654120656aac3
iyuokno <609753644@qq.com>
2024-01-31 09:35:06 +0800

    add label text

--- test/check_label.py
+++ test/check_label.py
@@ -8,8 +8,8 @@ description：检查yolo格式的标签是否正确
 
 import cv2
 
-img_path = r'D:\datasets\smoke_fire\dataset\images\dir1_smoke_00350.jpg'
-label_path = r'D:\datasets\smoke_fire\dataset\labels\dir1_smoke_00350.txt'
+img_path = r'D:\datasets\FallDetection\images\000029.jpg'
+label_path = r'D:\datasets\FallDetection\labels\000029.txt'
 
 img = cv2.imread(img_path)
 file = open(label_path,'r')
@@ -17,6 +17,9 @@ file = open(label_path,'r')
 
 
 lines = file.readlines()
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_size = 1
+font_color = (0, 0, 255)  # 白色，格式为 (B, G, R)
 
 for line in lines:
     info = line.split(' ')
@@ -32,6 +35,7 @@ for line in lines:
     x2 = cx + w/2
     y2 = cy + h/2
     cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 1)
+    cv2.putText(img, 'fall' if info[0] == '0' else 'up', (int(x1), int(y1)), font, font_size, font_color, 1)
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
