276a027a704d5c8277438b26e642ad80473954be
iyoukno <609753644@qq.com>
2024-08-13 22:53:24 +0800

    yolov5 infer update

--- yolov5_infer/pac/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-@Project ：interface.py 
-@File    ：__init__.py
-@Author  ：yuk
-@Date    ：2024/4/25 16:26 
-description：
-'''
---
+++ yolov5_infer_linux/interface.py
@@ -0,0 +1,7 @@
+'''
+@Project ：yolov5_infer_linux 
+@File    ：interface.py
+@Author  ：yuk
+@Date    ：2024/8/13 19:35 
+description：
+'''
